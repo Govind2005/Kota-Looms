c3493645683ce7eeb8538799f10e004a803dacd3
Govind2005 <realgovindkrishna@gmail.com>
2025-06-05 22:36:17 +0530

    updated updateuser

--- config/inngest.js
+++ config/inngest.js
@@ -35,10 +35,10 @@ export const syncUserUpdation = inngest.createFunction(
         event: 'clerk/user.updated'
     },
     async ({event}) => {
-        const { id, first_name, last_name, email_address, image_url} = event.data;
+        const { id, first_name, last_name, email_addresses, image_url} = event.data;
         const userData = {
             _id: id,
-            email: email_address[0].email_address,
+            email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             imageUrl: image_url
         }
