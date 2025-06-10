c083cc3beb6ff5a18ea4f488680ba21846009152
Govind2005 <realgovindkrishna@gmail.com>
2025-06-10 09:20:28 +0530

    bug fixed

--- config/inngest.js
+++ config/inngest.js
@@ -74,7 +74,7 @@ export const createUserOrder = inngest.createFunction(
         }
     },
     {event: 'order/created'},
-    async ({event}) => {
+    async ({events}) => {
         const orders = events.map((event) => {
             return {
                 userId: event.data.userId,
